21d9cae924d8077319839dd817caee0529111776
pup pup <92061071+puppuppuppup@users.noreply.github.com>
2024-12-19 17:15:39 +0300

    refactor: generics names

--- api/users.ts
+++ api/users.ts
@@ -1,18 +1,18 @@
 import { ApiInstance, getBaseApi, getExtendedApi } from '../utils/factories/api.factory';
 import { UserApi, UserCreate, UserUpdate } from '../types/users';
 
-interface IUsersApi extends ApiInstance<UserApi, UsersApi, UserCreate, UserUpdate> {
+interface IUsersApiExtended extends ApiInstance<UserApi, UsersApi, UserCreate, UserUpdate> {
     logEndpoint: () => void;
 }
 
 class UsersApi extends getBaseApi<UserApi, UsersApi, UserCreate, UserUpdate>('users') {}
 class UsersApiExtended
-    extends getExtendedApi<UserApi, UsersApi, UserCreate, UserUpdate, IUsersApi>('users')
-    implements IUsersApi
+    extends getExtendedApi<UserApi, UsersApi, UserCreate, UserUpdate, IUsersApiExtended>('users')
+    implements IUsersApiExtended
 {
     logEndpoint() {
         return;
-    }
+    };
 }
 
 UsersApi.getInstance().findMany();
--- utils/factories/api.factory.ts
+++ utils/factories/api.factory.ts
@@ -1,7 +1,7 @@
 import { AxiosInstance } from 'axios';
 import { HttpInstanceFactory } from './http-instance.factory';
 
-export abstract class ApiInstance<O, M, C, U> {
+export abstract class ApiInstance<SingleCopy, ManyCopies, CreateData, UpdateData> {
     protected httpInstance: AxiosInstance;
     protected endpoint: string;
 
@@ -10,34 +10,34 @@ export abstract class ApiInstance<O, M, C, U> {
         this.httpInstance = HttpInstanceFactory.getInstance();
     }
 
-    public async findOne(id: string | number): Promise<O> {
-        const res = await this.httpInstance.get<O>(`${this.endpoint}/${id}`);
+    public async findOne(id: string | number): Promise<SingleCopy> {
+        const res = await this.httpInstance.get<SingleCopy>(`${this.endpoint}/${id}`);
         return res.data;
     }
 
-    public async findMany(): Promise<M> {
-        const res = await this.httpInstance.get<M>(this.endpoint);
+    public async findMany(): Promise<ManyCopies> {
+        const res = await this.httpInstance.get<ManyCopies>(this.endpoint);
         return res.data;
     }
 
-    public async create(data: C): Promise<O> {
-        const res = await this.httpInstance.post<O>(this.endpoint, data);
+    public async create(data: CreateData): Promise<SingleCopy> {
+        const res = await this.httpInstance.post<SingleCopy>(this.endpoint, data);
         return res.data;
     }
 
-    public async update(id: string | number, data: U): Promise<O> {
-        const res = await this.httpInstance.put<O>(`${this.endpoint}/${id}`, data);
+    public async update(id: string | number, data: UpdateData): Promise<SingleCopy> {
+        const res = await this.httpInstance.put<SingleCopy>(`${this.endpoint}/${id}`, data);
         return res.data;
     }
 
-    public async delete(id: string | number): Promise<O> {
-        const res = await this.httpInstance.put<O>(`${this.endpoint}/${id}`);
+    public async delete(id: string | number): Promise<SingleCopy> {
+        const res = await this.httpInstance.put<SingleCopy>(`${this.endpoint}/${id}`);
         return res.data;
     }
 }
 
-export const getBaseApi = <O, M, U, C>(endpoint: string) => {
-    return class Api extends ApiInstance<O, M, U, C> {
+export const getBaseApi = <SingleCopy, ManyCopies, CreateData, UpdateData>(endpoint: string) => {
+    return class Api extends ApiInstance<SingleCopy, ManyCopies, CreateData, UpdateData> {
         private static instance: Api | null = null;
 
         public static getInstance(): Api {
@@ -49,15 +49,15 @@ export const getBaseApi = <O, M, U, C>(endpoint: string) => {
     };
 };
 
-export const getExtendedApi = <O, M, U, C, E extends ApiInstance<O, M, U, C>>(endpoint: string) => {
-    return class Api extends ApiInstance<O, M, U, C> {
-        private static instance: E | null = null;
+export const getExtendedApi = <SingleCopy, ManyCopies, CreateData, UpdateData, ExtendedApi extends ApiInstance<SingleCopy, ManyCopies, CreateData, UpdateData>>(endpoint: string) => {
+    return class Api extends ApiInstance<SingleCopy, ManyCopies, CreateData, UpdateData> {
+        private static instance: ExtendedApi | null = null;
 
-        public static getInstance(): E {
+        public static getInstance(): ExtendedApi {
             if (!this.instance) {
-                this.instance = new this(endpoint) as unknown as E;
+                this.instance = new this(endpoint) as unknown as ExtendedApi;
             }
-            return this.instance as E;
+            return this.instance as ExtendedApi;
         }
     };
 };
